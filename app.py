4a029ba0b46f93d833bbc0ca987934c7467e7716
sai pavani nagisetti <pavaninagisetti@sais-MBP.lan>
2020-09-29 00:37:14 -0700

    Password hashing and login api for authenticating user using werkzeug security

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # See <https://code-maven.com/using-templates-in-flask>
 
 from flask import Flask, request, jsonify, g
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import logging
 
@@ -43,6 +44,9 @@ def init_db():
         with app.open_resource('users.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+        
+
+#User registration
 
 @app.route('/register', methods=['POST'])
 def createUser():
@@ -51,13 +55,33 @@ def createUser():
     username = query_parameters.get('username')
     email = query_parameters.get('email')
     password = query_parameters.get('password')
+    hashed_password = generate_password_hash(password, "sha256") #Generating hash for the user entered password
     
     app.logger.info(username)
     
     db= get_db()
 
-    createNewUser = db.execute('INSERT INTO users (username, email, password) VALUES(?,?,?)',(username, email, password))
+    createNewUser = db.execute('INSERT INTO users (username, email, password) VALUES(?,?,?)',(username, email, hashed_password))
     res = db.commit()
     
     getusers = query_db('SELECT * FROM users')
     return jsonify(getusers)
+
+
+#Authenticating user
+
+@app.route('/login', methods=['POST'])
+def authenticateUser():
+    query_parameters = request.form
+    
+    username = query_parameters.get('username')
+    password = query_parameters.get('password')
+    
+    db= get_db()
+    result = query_db('SELECT password FROM users WHERE username = ?', [username])
+    
+    hashed_password = result[0].get('password')
+    return jsonify(check_password_hash(hashed_password, password)) #checking if user entered password is equal to the hashed password in db
+    
+    
+    
