3acb96bf99cf08957ee9ac02a49ad42b5917ca8b
sai pavani nagisetti <pavaninagisetti@sais-MBP.lan>
2020-10-05 23:05:57 -0700

    added test data

--- project2/schema.sql
+++ project2/schema.sql
@@ -25,5 +25,15 @@ CREATE TABLE Tweets(
     timestamp DATETIME,
 	FOREIGN KEY(username) REFERENCES users(username)
 );
-
+INSERT INTO users(username, email, password) VALUES('ProfAvery', 'bestprofessor@csu.fullerton.edu', 'KenyttCpSc449');
+INSERT INTO users(username, email, password) VALUES('Anna', 'Anna@gmail.com', 'ajwsh@ksdm');
+INSERT INTO users(username, email, password) VALUES('Pavani', 'nagisettipavani@gmail.com', 'aahd@g23!');
+INSERT INTO users(username, email, password) VALUES('Sruthi', 'sruthi@gmail.com', '04sru@1994');
+INSERT INTO users(username, email, password) VALUES('Sam', 'sam@gmail.com', 'samhash@akkineni');
+INSERT INTO users(username, email, password) VALUES('Rushabh', 'rushabh@gmail.com', 'rushabh9348$3');
+INSERT INTO user_following(username, usernameToFollow) VALUES('Pavani', 'Sam');
+INSERT INTO user_following(username, usernameToFollow) VALUES('Pavani', 'ProfAvery');
+INSERT INTO user_following(username, usernameToFollow) VALUES('Sruthi', 'Anna');
+INSERT INTO Tweets(username, text, timestamp) VALUES('Sruthi', 'Hey! this is my first tweet', '2020-10-06 06:03:47.989280');
+INSERT INTO Tweets(username, text, timestamp) VALUES('Anna', 'Super excited!', '2020-10-06 06:03:47.989280');
 COMMIT;
